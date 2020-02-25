1298e2bf4fb7268e0ee424dcfe26e2295faf7f65
andydlindsay@gmail.com <andydlindsay@gmail.com>
2020-02-25 12:17:36 -0800

    lecture end

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-console.log(process.argv);
+$(document).ready(() => {
+  const name = 'Andy';
+
+  const sayHello = (str) => {
+    console.log(`hello ${str}`);
+  };
+
+  const h1 = document.getElementById('title');
+  h1.addEventListener('mousemove', (event) => {
+    console.log(event.x, event.y);
+  });
+  console.log(h1);
+
+  const $h1 = jQuery('#title');
+  $h1.on('click', (event) => {
+    alert('hello');
+  });
+  console.log($h1);
+
+});
---
+++ script2.js
@@ -0,0 +1,3 @@
+// console.log(name);
+
+// sayHello('Francis');
